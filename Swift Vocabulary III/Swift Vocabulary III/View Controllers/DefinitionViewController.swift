f0e3509e791bc912999fb28218c314034231c2b1
Gerardo Hernandez <gahernandez02@gmail.com>
2021-06-15 15:04:36 -0700

    MVP

--- Swift Vocabulary III/Swift Vocabulary III/View Controllers/DefinitionViewController.swift
+++ Swift Vocabulary III/Swift Vocabulary III/View Controllers/DefinitionViewController.swift
@@ -8,29 +8,27 @@
 import UIKit
 
 class DefinitionViewController: UIViewController {
-
+    
+    // MARK: - Properties
     var vocabWord: VocabularyWord?
     
-    // UI
+    // MARK: - IBOutlets
     @IBOutlet var wordLabel: UILabel!
     @IBOutlet var definitionTextView: UITextView!
     
-    
+    // MARK: - Views
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Helper Methods
+    private func updateViews() {
+        guard let vocabWord = vocabWord else { return }
+        
+        wordLabel.text = vocabWord.word
+        definitionTextView.text = vocabWord.definition
     }
-    */
-
+    
 }
--- Swift Vocabulary III/Swift Vocabulary III/View Controllers/WordsTableViewController.swift
+++ Swift Vocabulary III/Swift Vocabulary III/View Controllers/WordsTableViewController.swift
@@ -9,39 +9,34 @@ import UIKit
 
 class WordsTableViewController: UITableViewController {
     
-    var vocabWords = [VocabularyWord]()
-
+    // MARK: - Properties
+    var vocabWords: [VocabularyWord] = [VocabularyWord(word: "Variable", definition: "A named value used to store information. Variables can change after being created."), VocabularyWord(word: "Constant", definition: "A named value used to store information. Constants cannot change after being created"), VocabularyWord(word: "Function", definition: "Functions are self-contained chunks of code that perform a specific task. You give a function a name that identifies what it does, and this name is used to “call” the function to perform its task when needed.") ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        
     }
-
+    
     // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return vocabWords.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
         
         let vocabularyWord = vocabWords[indexPath.row]
         cell.textLabel?.text = vocabularyWord.word
-
-        
         
         return cell
     }
-
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
         
         if segue.identifier == "ShowDefinitionSegue" {
             
@@ -51,9 +46,7 @@ class WordsTableViewController: UITableViewController {
             
             defitionVC.vocabWord = vocabularyWord
             
-            
-            
         }
     }
-
+    
 }
