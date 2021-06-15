d32c25c950c0e86cc9138062ec81c8c60f365699
Gerardo Hernandez <gahernandez02@gmail.com>
2021-06-15 14:35:25 -0700

    Created project file and added screens to the storyboard

---
+++ Swift Vocabulary III/Swift Vocabulary III/View Controllers/DefinitionViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DefinitionViewController.swift
+//  Swift Vocabulary III
+//
+//  Created by Gerardo Hernandez on 6/15/21.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    // UI
+    @IBOutlet var wordLabel: UILabel!
+    @IBOutlet var definitionTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
